eee39a18919038d2c31ac35f8dda05d1ab1933a8
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-10-30 18:02:01 -0300

    Creacion de componentes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material'
+import Navbar from "./components/Navbar";
+import VideoDetail from "./components/VideoDetail";
+import ChannelDetail from "./components/ChannelDetail";
+import SearchFeed from "./components/SearchFeed";
 
 const App = () => (
     <BrowserRouter>
