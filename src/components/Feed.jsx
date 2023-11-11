2ab6d6da0259226a0241512d6a1891c5849b3b1c
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-11-11 21:22:49 +0000

    Componente Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { Box, Stack, Typography } from '@mui/material'
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px:{ sx: 0, md: 2 } }}>
+        Sidebar
+        <Typography className='copyrigth' variant='body2' sx={{ mt: 1.5, color: '#fff'}}>
+          Copyright 2022 Santiago Quiroz
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
