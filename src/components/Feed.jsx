a2e59bb45f2b96873f3e9b1a312f353e7a3a8840
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-11-12 22:37:46 +0000

    Maquetado del componente Sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
 
+import Sidebar from './Sidebar'
+
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px:{ sx: 0, md: 2 } }}>
-        Sidebar
+        <Sidebar />
         <Typography className='copyrigth' variant='body2' sx={{ mt: 1.5, color: '#fff'}}>
           Copyright 2022 Santiago Quiroz
         </Typography>
