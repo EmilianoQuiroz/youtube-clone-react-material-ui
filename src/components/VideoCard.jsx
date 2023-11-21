e0aa4fcb7b21771177e972b2af9bfd36b7e9a99a
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-11-21 20:10:49 +0000

    Correccion de medidas

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -12,10 +12,10 @@ import { demoThumbnailUrl,
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
 
     return (
-    <Card sx={{ width: { md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: 0 }}>
+    <Card sx={{ width: {  xs: '100%', sm: '358px', md: '320px' }, boxShadow: 'none', borderRadius: 0 }}>
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} 
-        sx={{ width: { xs: '100%', sm: '358px'}, height: 180 }} 
+        sx={{ width: { xs: '100%', sm: '358px', md: '320px' }, height: 180 }} 
       />
         </Link>
 
